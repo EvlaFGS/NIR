cc7315cd5ff0e1e012889a74ba08b872b528b5b3
imitrichev <imitrichev@github.com>
2022-03-24 23:13:28 +0300

    fix ellipse size (to user input) in fill.m

--- fill.m
+++ fill.m
@@ -5,12 +5,16 @@ ii=[];
 
  A = dlmread('Coord.txt','\t',1,0)
 
+prompt = "Enter A_C in mkm: ";
+A_C = input(prompt)
+prompt = "Enter B in mkm: ";
+B = input(prompt)
 
 for i=1:1:30
 for x=-10:0.6:10
 for y=-10:0.6:10
 for z=-10:0.6:10
-if ((x-A(i,1)).^2/4.0+(y-A(i,2)).^2/4.0+(z-A(i,3)).^2/4.0<1.0)
+if ((x-A(i,1)).^2/(A_C.^2)+(y-A(i,2)).^2/(B.^2)+(z-A(i,3)).^2/(A_C.^2)<1.0)
 xx=[xx x];
 yy=[yy y];
 zz=[zz z];
